c7e50b3bd651ec246daa21d9bbb1cc65bc640865
Somnath Saha <saha.somnath@gmail.com>
2019-10-12 23:48:30 -0700

    Initial checkin of script for publishing message

--- Python/redis_cluster_publish.py
+++ Python/redis_cluster_publish.py
@@ -1,1 +1,42 @@
 #!/usr/bin/python
+
+import os,sys
+import argparse
+import textwrap
+from redis_cluster import RedisCluster
+
+
+def commandLineArguments():
+    """
+    Process command line arguments
+    :return: <dict> A dictionary of parameters
+    """
+    desc = textwrap.dedent('''
+    Publish a message to a channel/queue.
+    cmd:
+    $python Redis_Cluster_Publish.py -c <channel> -m <message>
+    ''')
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description=desc)
+    parser.add_argument('--channel', '-c', default="CH", help="Channel Name")
+    parser.add_argument('--message', '-m', help="Message in JSON format")
+
+    args = vars(parser.parse_args())
+    print(args)
+    return args
+
+def main():
+
+    args = commandLineArguments()
+    channel = args["channel"]
+    message = args["message"]
+
+    # Invoke queue and message
+    rc = RedisCluster()
+    print(rc.ping())
+    rc.publish(channel, message)
+	
+
+
+
+if __name__ == "__main__":
+    main()
